187c98210a15cb6329938fc7279760847fdb4984
Nasiruddin Ansari <43772329+nasir97177@users.noreply.github.com>
2024-11-07 16:02:04 +0300

    api.py

--- ksa_localization/api.py
+++ ksa_localization/api.py
@@ -1,1 +1,14 @@
+import frappe
+
+def create_accrued_entries(doc, method):
+    accrued_doctypes = ["Accrued Gosi", "Accrued Ticket", "Accrued Vacation", "Accrued End of Service"]
+    
+    for doctype in accrued_doctypes:
+        pr = frappe.get_doc({
+            "doctype": doctype,
+            "employee": doc.employee,
+            "date": doc.posting_date,
+        })
+        pr.insert()
+        pr.submit()
 
